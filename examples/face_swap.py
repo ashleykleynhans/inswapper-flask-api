9cd6d7bb8c0ba4c7e13193618330e7e20b72526d
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-07-09 00:01:02 +0200

    Cleaned up example

--- examples/face_swap.py
+++ examples/face_swap.py
@@ -10,30 +10,25 @@ URL = 'http://127.0.0.1:8000/faceswap'
 SOURCE_IMAGE = '/Users/ashley/src/inswapper/data/src.jpg'
 TARGET_IMAGE = '/Users/ashley/src/inswapper/data/target.jpg'
 
-payload = {
-    'request_id': 0,
-    'user_id': 0,
-    'event_id': 0
-}
 
-files = {
-    'source_image': open(SOURCE_IMAGE, 'rb'),
-    'target_image': open(TARGET_IMAGE, 'rb')
-}
+if __name__ == '__main__':
+    files = {
+        'source_image': open(SOURCE_IMAGE, 'rb'),
+        'target_image': open(TARGET_IMAGE, 'rb')
+    }
 
-r = requests.post(
-    URL,
-    data=payload,
-    files=files
-)
+    r = requests.post(
+        URL,
+        files=files
+    )
 
-print(f'HTTP status code: {r.status_code}')
+    print(f'HTTP status code: {r.status_code}')
 
-resp_json = r.json()
+    resp_json = r.json()
 
-img = Image.open(io.BytesIO(base64.b64decode(resp_json['image'])))
-output_file = f'{uuid.uuid4()}.jpg'
+    img = Image.open(io.BytesIO(base64.b64decode(resp_json['image'])))
+    output_file = f'{uuid.uuid4()}.jpg'
 
-with open(output_file, 'wb') as f:
-    print(f'Saving image: {output_file}')
-    img.save(f, format='JPEG')
+    with open(output_file, 'wb') as f:
+        print(f'Saving image: {output_file}')
+        img.save(f, format='JPEG')
